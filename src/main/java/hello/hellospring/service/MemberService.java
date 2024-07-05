a65f8b5907fd7009f0a774475efd229973a58c3f
ziyesss <choijiyea7185@gmail.com>
2024-07-05 20:00:03 +0900

    (리팩토링) 회원 서비스 개발: 회원가입

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -14,11 +14,15 @@ public class MemberService {
     public Long join(Member member) {
 
         //규칙 1) 같은 이름이 있는 중복 회원은 안된다.
-        Optional<Member> result = memberRepository.findByName(member.getName());
-        result.ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다. ");
-        });
+        validateDuplicateMember(member); // 중복회원 검증
         memberRepository.save(member);
         return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다. ");
+                });
     }
 }
