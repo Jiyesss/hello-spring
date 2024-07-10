3efba2a53b85d6b07ff5fefc467212c121e8da08
ziyesss <choijiyea7185@gmail.com>
2024-07-10 14:55:56 +0900

    JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,17 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.*;
+
+//JPA가 관리하는 entity라는 표시를 나타내는 어노테이션
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
+
+    //@Column(name= "username")
     private String name;
 
     public Long getId() {
